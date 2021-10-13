044c9eefa9261c354f925f79fb1d3e3d15bad3b4
Nick <nandreala@gmail.com>
2021-10-12 23:05:43 -0400

    added work around for gitpages using sveltekit

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+import { publish } from 'gh-pages';
+
+publish(
+	'build', // path to public directory
+	{
+		branch: 'gh-pages',
+		repo: 'https://github.com/JovianDev/SYLPH-WEB.git', // Update to point to your repository
+		user: {
+			name: 'Nick Andreala',
+			email: 'nandreala@gmail.com'
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,15 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
